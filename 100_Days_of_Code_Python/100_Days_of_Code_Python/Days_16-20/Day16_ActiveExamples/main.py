c5762280e6dc22b575528e527075ecfc9e0e246c
Bradley Staron <75392007+bradleystaron@users.noreply.github.com>
2025-12-09 16:05:45 -0500

    Commented out Turtle; Added PrettyTable

--- 100_Days_of_Code_Python/100_Days_of_Code_Python/Days_16-20/Day16_ActiveExamples/main.py
+++ 100_Days_of_Code_Python/100_Days_of_Code_Python/Days_16-20/Day16_ActiveExamples/main.py
@@ -1,16 +1,30 @@
-import another_module
-from turtle import *
+# import another_module
+# from turtle import *
 
-print(another_module.another_variable)  
+# print(another_module.another_variable)  
 
-# new object named timmy
-timmy = Turtle()
+# # new object named timmy
+# timmy = Turtle()
 
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+# This Creates a new PrettyTable object
+table = PrettyTable()
+
+# This taps into an objects method to add columns to the table
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# This taps into an objects attribute to change the alignment of the table
+table.align = "l"
+
+print(table)
