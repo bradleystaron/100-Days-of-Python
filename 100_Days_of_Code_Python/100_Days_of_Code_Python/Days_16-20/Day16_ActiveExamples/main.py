17bc163f1965ba5bc678a39e3e02a61bafda69e0
Bradley Staron <75392007+bradleystaron@users.noreply.github.com>
2025-12-09 11:48:12 -0500

    Turtle Object Added

--- 100_Days_of_Code_Python/100_Days_of_Code_Python/Days_16-20/Day16_ActiveExamples/main.py
+++ 100_Days_of_Code_Python/100_Days_of_Code_Python/Days_16-20/Day16_ActiveExamples/main.py
@@ -1,3 +1,16 @@
 import another_module
+from turtle import *
 
 print(another_module.another_variable)  
+
+# new object named timmy
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
